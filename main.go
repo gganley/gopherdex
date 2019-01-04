8cba0111f6c32dd6a4f77c414f10ede6120a1238
Chan Young <c.young.koh@gmail.com>
2019-01-04 18:34:16 -0500

    database step 1

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+//basic attribute for pokemon
+type Pokemon struct {
+	Id     int64
+	Images map[string]Image
+}
+
+//basic attribute for image size
+type Image struct {
+	URL    string
+	Width  int32
+	Height int32
+}
 
 func main() {
 
-	sql.Open()
+	db, err := sql.Open("sqlite3", "./poko.db")
+	//checkErr(err)
+	if err != nil {
+		panic(err)
+	}
+	db.Begin()
+
+	fmt.Println()
 
 }
