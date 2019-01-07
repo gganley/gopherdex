8b2e26d2c3455a91372a18672c30eed1da304a23
gganley <gganley@student.bridgew.edu>
2019-01-06 22:14:08 -0500

    Slurp in pokemon.json file

---
+++ slurp_pokemon.go
@@ -0,0 +1,49 @@
+package gopherdex
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"fmt"
+)
+
+type Pokedex struct {
+	ID int
+	Name Name
+	Type []string
+	Base Base
+}
+
+type Name struct {
+	English string
+	Japanese string
+	Chinese string
+}
+
+type Base struct {
+	HP int
+	Attack int
+	Defense int
+	SpAttack int
+	SpDefense int
+	Speed int
+}
+
+
+func getPokedex() []Pokedex {
+	var m []Pokedex
+	data, err := ioutil.ReadFile("pokemon.json/pokedex.json")
+
+	if err != nil {
+		panic(err)
+	}
+
+    unmarshalErr := json.Unmarshal(data, &m)
+
+	if unmarshalErr != nil {
+		panic(unmarshalErr)
+	}
+
+	fmt.Printf("+%v\n", m)
+
+	return(m)
+}
